4c6f1446356735332674873fa43b809910b9f359
francisco <allthegameshere@outlook.com>
2023-08-05 01:49:33 -0400

    added css, created paint object and added some html, still not complete html and css

---
+++ paintProject/script/script.js
@@ -0,0 +1,16 @@
+let myPaints = [];
+
+function Paints(color, brand, size, quantity){
+    this.color=color;
+    this.brand=brand;
+    this.size=size;
+    this.quantity=quantity;
+
+
+};
+
+function addToList(){
+    myPaints.push();
+
+};  
+
